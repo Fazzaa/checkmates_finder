4242afb416d2c4f8e743e8391ca09db6f84b982c
andreafancellu <andreafancellu123@gmail.com>
2023-03-17 17:45:43 +0100

    check check pawn right and left

--- dominio_1.pl
+++ dominio_1.pl
@@ -23,18 +23,36 @@ nero(pedone_nero_2).
 nero(pedone_nero_3).
 
 initialize :- 
+    retractall(occupata(_,_)),
     assert(occupata(pos(8,7), re_nero)),
     assert(occupata(pos(7,6), pedone_nero_1)),
     assert(occupata(pos(7,7), pedone_nero_2)),
-    assert(occupata(pos(7,8), pedone_nero_3)),
+    % assert(occupata(pos(7,8), pedone_nero_3)),
     assert(occupata(pos(6,8), pedone_bianco_1)),
-    assert(occupata(pos(1,1), pedone_bianco_2)),
+    % assert(occupata(pos(1,1), pedone_bianco_2)),
+    assert(occupata(pos(7,6), pedone_bianco_2)),
     assert(occupata(pos(1,5), torre_bianco)),
     assert(occupata(pos(1,6), re_bianco)).
 
 %checkmate(pos(8,5), torreBianco)%
 checkmate(X,Y) :- 
-    check(X,Y), 
+    check, 
     \+mangiabile(X), 
     \+move(reNero), 
     \+opposizione().
+
+check_pawn_right :-
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    RigaPedone is RigaRe-1,
+    ColonnaPedone is ColonnaRe +1,
+    pedone(Pedone),
+    bianco(Pedone),
+    occupata(pos(RigaPedone, ColonnaPedone), Pedone).
+
+check_pawn_left :-
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    RigaPedone is RigaRe-1,
+    ColonnaPedone is ColonnaRe -1,
+    pedone(Pedone),
+    bianco(Pedone),
+    occupata(pos(RigaPedone, ColonnaPedone), Pedone).

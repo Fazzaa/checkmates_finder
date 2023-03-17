f38ff15336b28cf86d28fd05e80b69f0d4409469
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-17 16:40:45 +0100

    added domain backrank checkmate.

---
+++ dominio_1.pl
@@ -0,0 +1,33 @@
+num_colonne(8).
+num_righe(8).
+
+pedone(pedone_nero_1).
+pedone(pedone_nero_2).
+pedone(pedone_nero_3).
+re(re_nero).
+re(re_bianco).
+torre(torre_bianco).
+
+bianco(torre_bianco).
+bianco(re_bianco).
+
+nero(re_nero).
+nero(pedone_nero_1).
+nero(pedone_nero_2).
+nero(pedone_nero_3).
+
+
+occupata(pos(8,7), re_nero).
+occupata(pos(7,6), pedone_nero_1).
+occupata(pos(7,7), pedone_nero_2).
+occupata(pos(7,8), pedone_nero_3).
+
+occupata(pos(1,5), torre_bianco).
+occupata(pos(1,6), re_bianco).
+
+%checkmate(pos(8,5), torreBianco)%
+checkmate(X,Y) :- 
+    check(X,Y), 
+    \+mangiabile(X), 
+    \+move(reNero), 
+    \+opposizione().

d31ac4412d89f5ac7f1d676354f874afaeba714e
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-17 16:45:13 +0100

    added white pawn

--- dominio_1.pl
+++ dominio_1.pl
@@ -4,10 +4,13 @@ num_righe(8).
 pedone(pedone_nero_1).
 pedone(pedone_nero_2).
 pedone(pedone_nero_3).
+pedone(pedone_bianco_1).
 re(re_nero).
 re(re_bianco).
 torre(torre_bianco).
 
+
+bianco(pedone_bianco_1).
 bianco(torre_bianco).
 bianco(re_bianco).
 
@@ -21,6 +24,7 @@ occupata(pos(8,7), re_nero).
 occupata(pos(7,6), pedone_nero_1).
 occupata(pos(7,7), pedone_nero_2).
 occupata(pos(7,8), pedone_nero_3).
+occupata(pos(1,1), pedone_bianco_1).
 
 occupata(pos(1,5), torre_bianco).
 occupata(pos(1,6), re_bianco).

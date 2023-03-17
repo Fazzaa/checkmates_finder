1e114359bf2bab4f2248abb8d548b7d548a6cd39
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-17 16:48:05 +0100

    pawn maybe can move now

--- azioni.pl
+++ azioni.pl
@@ -2,7 +2,7 @@ applicabile(su, Pedone, pos(Riga, Colonna)) :-
     Riga < 8,
     bianco(Pedone),
     RigaSopra is Riga + 1,
-    \+ occupata(pos(RigaSopra, Colonna)).
+    \+ occupata(_, pos(RigaSopra, Colonna)).
 
 trasforma(su, Pedone, pos(Riga, Colonna), pos(RigaSopra, Colonna)):-
     RigaSopra is Riga + 1.

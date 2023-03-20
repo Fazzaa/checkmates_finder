a7582a90dc81ec0683a0576349d0687c1cefa4b4
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-20 15:00:37 +0100

    added check rules for the rook

--- dominio_1.pl
+++ dominio_1.pl
@@ -32,11 +32,12 @@ initialize :-
     assert(occupata(pos(7,8), pedone_nero_3)),
     assert(occupata(pos(5,3), pedone_nero_4)),
     assert(occupata(pos(6,8), pedone_bianco_1)),
-    assert(occupata(pos(6,5), pedone_bianco_2)),
-    assert(occupata(pos(1,4), torre_bianco)),
+    assert(occupata(pos(2,5), pedone_bianco_2)),
+    assert(occupata(pos(1,1), torre_bianco)),
     assert(occupata(pos(1,5), re_bianco)).
 
-%checkmate(pos(8,5), torreBianco)%
+
+%checkmate(pos(1,1), torreBianco)%
 checkmate(X,_) :- 
     check, 
     \+mangiabile(X), 
@@ -58,3 +59,26 @@ check_pawn_left :-
     pedone(Pedone),
     bianco(Pedone),
     occupata(pos(RigaPedone, ColonnaPedone), Pedone).
+
+
+check_rook_row :-
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    torre(Torre),
+    bianco(Torre),
+    occupata(pos(RigaRe, ColonnaTorre), Torre),
+    N is (ColonnaRe-1)-ColonnaTorre,
+    applicabile(dx, Torre, pos(RigaRe, ColonnaTorre), N).
+    %ColonnaAfterTorre is ColonnaTorre + 1,
+    %ColonnaBeforeRe is ColonnaRe - 1,
+    %riga_occupata(pos(RigaRe, ColonnaAfterTorre), pos(RigaRe, ColonnaBeforeRe)).    
+
+check_rook_col :-
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    torre(Torre),
+    bianco(Torre),
+    occupata(pos(RigaTorre, ColonnaRe), Torre),
+    N is (RigaRe-1)-RigaTorre,
+    applicabile(su, Torre, pos(RigaTorre, ColonnaRe), N).
+    %RigaAfterTorre is RigaTorre + 1,
+    %RigaBeforeRe is RigaRe - 1,
+    %colonna_occupata(pos(RigaAfterTorre, ColonnaRe), pos(RigaBeforeRe, ColonnaRe)).

fc930aa1fe12de3c3a89821232c2646970e16a1a
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-20 17:33:54 +0100

    IT FINDS MATE IN 1

---
+++ ida*.pl
@@ -0,0 +1,19 @@
+prova(N):-
+    risolvi(N).
+
+prova(N):-
+    NNuovo is N+1,
+    NNuovo<8,
+    prova(NNuovo).
+
+risolvi(N):-
+    initialize,
+    bianco(Pezzo),
+    occupata(pos(X,Y), Pezzo),
+    applicabile(Az, Pezzo, pos(X,Y), N),
+    trasforma(Az, Pezzo, pos(X,Y), N),
+    check(Pezzo),
+    %length(Az, L),
+    %write(L)
+    write(Az),write("---"), write(Pezzo),write("---"), write(N), write("\n").
+

6d29867bb812f1c6a1a8d1b70b9758f573c2accf
andreafancellu <andreafancellu123@gmail.com>
2023-03-22 00:52:15 +0100

    risolvi for black movement. found more problems

--- idastar.pl
+++ idastar.pl
@@ -1,12 +1,12 @@
-prova(N):-
-    risolvi(N).
+prova_bianco(N):-
+    risolvi_bianco(N).
 
-prova(N):-
+prova_bianco(N):-
     NNuovo is N+1,
     NNuovo<8,
-    prova(NNuovo).
+    prova_bianco(NNuovo).
 
-risolvi(N):-
+risolvi_bianco(N):-
     initialize,
     bianco(Pezzo),
     occupata(pos(X,Y), Pezzo),
@@ -15,7 +15,24 @@ risolvi(N):-
     check(Pezzo),
     %length(Az, L),
     %write(L)
-    write(Az),write("---"), write(Pezzo),write("---"), write(N), write("\n"),
-    read(String),
-    String.
+    write(Az),write("---"), write(Pezzo),write("---"), write(N), write("\n"), !.
+    %read(String),
+    %String.
 
+prova_nero(N) :-
+    risolvi_nero(N).
+
+prova_nero(N) :-
+    NNuovo is N+1,
+    NNuovo < 2,
+    prova_nero(NNuovo).
+
+risolvi_nero(N) :-
+    % problema, qui non si può fare l'initialize ogni volta, pena vanificare tutto ciò che ha fatto il bianco per scegliere la mossa. Si potrebbero salvare le mosse del bianco in una lista e fare una "update" per il nero che legge dalla lista e ricompone scacchiera dal punto in cui l'ha lasciata il bianco
+    nero(Pezzo),
+    re(Pezzo),
+    occupata(pos(X,Y), Pezzo),
+    applicabile(Az, Pezzo, pos(X,Y), N),
+    trasforma(Az, Pezzo, pos(X,Y), N),
+    \+check(torre_bianco),
+    write(Az),write("---"), write(N), write("---"), write("no more check motherfuckers").

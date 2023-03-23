15a5129a0b79ac12ad13993d8f3dbd84b3352c31
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-23 17:36:25 +0100

    first working position

--- dominio_1.pl
+++ dominio_1.pl
@@ -3,27 +3,21 @@ num_righe(8).
 
 pedone(pedone_nero_1).
 pedone(pedone_nero_2).
-pedone(pedone_nero_3).
 pedone(pedone_nero_4).
-pedone(pedone_bianco_1).
-pedone(pedone_bianco_2).
+pedone(pedone_bianco).
 re(re_nero).
 re(re_bianco).
 torre(torre_bianco).
-torre(torre_bianco_2).
 
 
-bianco(pedone_bianco_1).
-bianco(pedone_bianco_2).
+bianco(pedone_bianco).
 bianco(torre_bianco).
-bianco(torre_bianco_2).
 bianco(re_bianco).
 
 
 nero(re_nero).
 nero(pedone_nero_1).
 nero(pedone_nero_2).
-nero(pedone_nero_3).
 nero(pedone_nero_4).
 
 initialize :- 
@@ -31,14 +25,10 @@ initialize :-
     assert(occupata(pos(8,7), re_nero)),
     assert(occupata(pos(7,6), pedone_nero_1)),
     assert(occupata(pos(6,7), pedone_nero_2)),
-    assert(occupata(pos(7,8), pedone_nero_3)),
     assert(occupata(pos(5,3), pedone_nero_4)),
-    assert(occupata(pos(5,8), pedone_bianco_1)),
-    assert(occupata(pos(2,5), pedone_bianco_2)),
+    assert(occupata(pos(6,6), pedone_bianco)),
     assert(occupata(pos(1,1), torre_bianco)),
-    assert(occupata(pos(1,2), torre_bianco_2)),
     assert(occupata(pos(1,5), re_bianco)).
-
 
 %checkmate(pos(1,1), torreBianco)%
 checkmate(X,_) :- 
@@ -47,32 +37,68 @@ checkmate(X,_) :-
     \+move(reNero), 
     \+opposizione().
 
-check(Pezzo):-
+check(Pezzo, pos(X,Y), _):-
     pedone(Pezzo),
     occupata(pos(RigaRe, ColonnaRe), re_nero),
-    RigaPedone is RigaRe-1,
-    ColonnaPedone is ColonnaRe +1,
-    occupata(pos(RigaPedone, ColonnaPedone), Pezzo).
+    XNuovo is X+2,
+    RigaRe = XNuovo,
+    YConfuso is Y + 1,
+    ColonnaRe = YConfuso.
 
-check(Pezzo):-
+check(Pezzo, pos(X,Y), _):-
     pedone(Pezzo),
     occupata(pos(RigaRe, ColonnaRe), re_nero),
-    RigaPedone is RigaRe-1,
-    ColonnaPedone is ColonnaRe -1,
-    occupata(pos(RigaPedone, ColonnaPedone), Pezzo).
+    XNuovo is X+2,
+    RigaRe = XNuovo,
+    YConfuso is Y -1,
+    ColonnaRe = YConfuso.
 
-
-check(Pezzo):-
+check(Pezzo, pos(X,Y), N):-
     torre(Pezzo),
     occupata(pos(RigaRe, ColonnaRe), re_nero),
-    occupata(pos(RigaRe, ColonnaTorre), Pezzo),
-    N is (ColonnaRe-1)-ColonnaTorre,
-    applicabile(dx, Pezzo, pos(RigaRe, ColonnaTorre), N).
+    XNuovo is X + N, %sposto la torre su
+    XNuovo = RigaRe,
+    Diff is abs((ColonnaRe-1)-Y),
+    applicabile(dx, Pezzo, pos(XNuovo, Y), Diff).
 
-check(Pezzo) :-
+check(Pezzo, pos(X,Y), N):-
     torre(Pezzo),
     occupata(pos(RigaRe, ColonnaRe), re_nero),
-    occupata(pos(RigaTorre, ColonnaRe), Pezzo),
-    N is (RigaRe-1)-RigaTorre,
-    applicabile(su, Pezzo, pos(RigaTorre, ColonnaRe), N).
+    XNuovo is X - N, %riga giù
+    XNuovo = RigaRe,
+    Diff is abs((ColonnaRe-1)-Y),
+    applicabile(dx, Pezzo, pos(XNuovo, Y), Diff).
 
+check(Pezzo, pos(X,Y), N):-
+    torre(Pezzo),
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    YNuovo is Y + N, %muovo la torre a dx
+    YNuovo = ColonnaRe,
+    Diff is abs((RigaRe-1)-X),
+    applicabile(su, Pezzo, pos(X, YNuovo), Diff).
+
+check(Pezzo, pos(X,Y), N):-
+    torre(Pezzo),
+    occupata(pos(RigaRe, ColonnaRe), re_nero),
+    YNuovo is Y - N, %muovo la torre a sx
+    YNuovo=ColonnaRe,
+    Diff is abs((RigaRe-1)-X),
+    applicabile(su, Pezzo, pos(X, YNuovo), Diff).
+
+
+
+no_check(pos(XRe,YRe)) :-
+    torre(TB),
+    bianco(TB),
+    occupata(pos(XB, YB), TB),
+    \+ XRe = XB,
+    \+ YRe = YB,
+    XPedone is XRe - 1,
+    YPedoneSx is YRe - 1,
+    YPedoneDx is YRe + 1,
+    pedone(PB),
+    bianco(PB),
+    \+occupata(pos(XPedone, YPedoneSx), PB),
+    \+occupata(pos(XPedone, YPedoneDx), PB).
+
+    

faabe6b60f7f51234e7f7271b58e7e358e3ebd7a
Fabio Luciani <lu.fa.98@gmail.com>
2023-03-23 17:34:56 +0100

    fixed rounds for black and white

--- idastar.pl
+++ idastar.pl
@@ -4,23 +4,16 @@ prova_bianco(N):-
 prova_bianco(N):-
     NNuovo is N+1,
     NNuovo<8,
-    
     prova_bianco(NNuovo).
 
 risolvi_bianco(N):-
-    initialize,
-    bianco(Pezzo),
-    occupata(pos(X,Y), Pezzo),
-    applicabile(Az, Pezzo, pos(X,Y), N),
-    trasforma(Az, Pezzo, pos(X,Y), N),
-    check(Pezzo),
-    %retract(occupata, Pezzo)
-    %length(Az, L),
-    %write(L)
-    write(Az),write("---"), write(Pezzo),write("---"), write(N), write("\n"), !. % usa la prima mossa trovata che dà scacco
-    %read(String),
-    %String.
-
+    bianco(PezzoBianco),
+    occupata(pos(XBianco,YBianco), PezzoBianco),
+    applicabile(AzBianco, PezzoBianco, pos(XBianco,YBianco), N),
+    check(PezzoBianco, pos(XBianco,YBianco), N),
+    trasforma(AzBianco, PezzoBianco, pos(XBianco,YBianco), N),
+    write(AzBianco),write("---"), write(PezzoBianco),write("---"), write(N), write("\n"), !. % usa la prima mossa trovata che dà scacco
+    
 prova_nero(N) :-
     risolvi_nero(N).
 
@@ -29,13 +22,13 @@ prova_nero(N) :-
     NNuovo < 2,
     prova_nero(NNuovo).
 
+prova_nero(_) :-
+    write("Scaccomatto!").
+
 risolvi_nero(N) :-
-    % problema, qui non si può fare l'initialize ogni volta, pena vanificare tutto ciò che ha fatto il bianco per scegliere la mossa. Si potrebbero salvare le mosse del bianco in una lista e fare una "update" per il nero che legge dalla lista e ricompone scacchiera dal punto in cui l'ha lasciata il bianco
-    nero(Pezzo),
-    re(Pezzo),
-    occupata(pos(X,Y), Pezzo),
-    applicabile(Az, Pezzo, pos(X,Y), N),
-    trasforma(Az, Pezzo, pos(X,Y), N),
-    bianco(PezzoBianco),
-    \+check(PezzoBianco), %pezzoBianco
-    write(Az),write("---"), write(N), write("---"), write("no check motherfuckers").
+    nero(PezzoNero),
+    re(PezzoNero),
+    occupata(pos(XNero,YNero), PezzoNero),
+    applicabile(AzNero, PezzoNero, pos(XNero,YNero), N), %pezzoBianco
+    trasforma(AzNero, PezzoNero, pos(XNero,YNero), N),
+    write(AzNero),write("---"), write(N), write("---"), write("no check motherfuckers"), write("\n").
